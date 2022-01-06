dc78b887494dca3b15c3d5c69a25c423138ec764
ArpitaX <95682842+ArpitaX@users.noreply.github.com>
2022-01-06 14:46:03 +0530

    Update forward.py

--- aditya/modules/forward.py
+++ aditya/modules/forward.py
@@ -1,1 +1,11 @@
-1
+from pyrogram import filters
+from aditya import adi
+import asyncio
+import os
+
+from_grup=os.getenv("FROM_GROUP")
+to_grup=os.getenv("TO_GROUP")
+
+@adi.on_message(filters.chat(from_grup))
+async def pm(client, message):
+  await message.forward(to_grup)
