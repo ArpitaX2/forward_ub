601e683e681637531a1e0ee690919b0a62a2fcaf
ArpitaX <95682842+ArpitaX@users.noreply.github.com>
2022-01-07 08:20:44 +0530

    Update forward.py

--- aditya/modules/forward.py
+++ aditya/modules/forward.py
@@ -6,6 +6,10 @@ import os
 from_grup=os.getenv("FROM_GROUP")
 to_grup=os.getenv("TO_GROUP")
 
+@adi.on_message(filters.command("ping", ",") & filters.me)
+async def ping(client, message):
+    await message.edit_text("🏓Pong!")
+
 @adi.on_message(filters.chat(from_grup))
 async def pm(client, message):
   await message.forward(to_grup)
